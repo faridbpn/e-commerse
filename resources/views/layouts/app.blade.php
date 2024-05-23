17b3fcf3001802f986a43266f8c0e6ad8ee2a701
farid.bpn <farid.IDNSOLO@gmail.com>
2024-05-23 08:19:32 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>@yield('title')</title>
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link href="style/main.css" rel="stylesheet" />
+
+
+    <!-- style -->
+    @stack('prepend-style')
+    @include('includes.style')
+    @stack('addon-style')
+
+
+
+  </head>
+
+  <body>
+    
+    <!-- navbar -->
+    <!-- cara manggil nya pakai titik + nama yang mau di panggil -->
+    @include('includes.navbar')
+
+
+
+    <!-- Page Content -->
+    @yield('content')
+
+
+    <!-- footer -->
+    @include('includes.footer')
+    
+
+    <!-- Bootstrap core JavaScript -->
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
+    
+
+    
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
+Route::get('/details/{id}', [App\Http\Controllers\DetailController::class, 'index'])->name('detail');
+
+
+Auth::routes();
